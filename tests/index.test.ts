0fa8ef27933e2f2449ec5d44fa0a996512f38195
ZaK3939 <tsu.aria.kalei@gmail.com>
2023-01-14 17:18:55 +0400

    first commit

---
+++ tests/index.test.ts
@@ -0,0 +1,10 @@
+import { hello } from '../src';
+
+describe('hello() のテスト', () => {
+  it('Hello. と出力', () => {
+    const log = jest.spyOn(console, 'log').mockReturnValue();
+    hello();
+    expect(log).toHaveBeenNthCalledWith(1, 'Hello.');
+    log.mockRestore();
+  });
+});
